04cc40a46a41c7443f34de12ce1a56ebc4becfff
shaikhrafiaan9 <shaikhrafiaan@gamil.com>
2020-12-02 21:46:14 +0530

    staticfiles

--- StackOverFlow/settings.py
+++ StackOverFlow/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
 
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -119,6 +120,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 
 STATIC_URL = '/static/'
